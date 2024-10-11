bec997b6af0935f8721465fe382c177a464b2c65
Arthur-P-V <apetersonveatch@gmail.com>
2024-10-11 02:52:33 -0500

    Adds roughs of spawner, Highscore, and Leaderboard

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.UIElements;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject GameOver;
+
+    public static GameManager Instance;
+    public GameObject GameOverText;
+    public bool GameOver = false;
+    public bool saved = false;
     public int score = 0;
+    public int bestScore;
     public PlayerController Player;
     public float scoreInterval = 0.05f;
     public TextMeshProUGUI scoreText;
-    
+    public TextMeshProUGUI highScoreText;
+
+    public GameObject Leaderboard;
+    public List<TextMeshProUGUI> Scores = new List<TextMeshProUGUI>();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     void Start()
     {
+        bestScore = PlayerPrefs.GetInt("0", 0); //Grabs the highest score (index 0) or returns 0
+        highScoreText.text = "Best: " + bestScore;
         StartCoroutine("TickScore");
     }
 
@@ -23,18 +41,57 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         scoreText.text = "Score: " + score;
+
+        if (score > bestScore) {
+            highScoreText.text = "Best: " + score;
+        }
+
+
+
         if (Player.dead) {
-            GameOver.SetActive(true);
+            GameOverText.SetActive(true);
+            GameOver = true;
             if (Input.GetKeyDown(KeyCode.R)) {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
         }
+
+        if (GameOver && !saved)
+        {
+            for (int i = 0; i < 5; i++) {
+                if (score > PlayerPrefs.GetInt($"{i}", 0)) {
+                    PlayerPrefs.SetInt($"{i}", score);
+                    PlayerPrefs.Save();
+                    break;
+                    //Scores[i].text = $"{i}. {PlayerPrefs.GetInt($"{i}")}";
+                }
+                //else
+                //{
+                //    Scores[i].text = $"{i}. {PlayerPrefs.GetInt($"{i}", 0)}";
+                //}
+            }
+            Leaderboard.SetActive(true);
+            saved = true;
+            DisplayLeaderboard();
+        }
+
+    }
+
+    public void DisplayLeaderboard() {
+        for (int i = 0; i < 5; i++)
+        {
+            Scores[i].text = $"{i}. {PlayerPrefs.GetInt($"{i}", 0)}";
+        }
+    
     }
 
     public IEnumerator TickScore() {
         while (true)
         {
-            score += 1;
+            if (!Player.dead)
+            {
+                score += 1;
+            }
             yield return new WaitForSeconds(scoreInterval);
         }
         
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Subsystems;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject Player;
+    public GameObject Bar;
+    public float spawnInterval = 1f;
+
+    private List<GameObject> obstacleQueue = new List<GameObject>();
+    
+    private GameObject pool;
+
+    //Tag objects with a level and spawn ALL OF THEM in the beginning? then as the player goes through the levels more objects are added to the pool for instantiation giving variety
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pool = transform.GetChild(0).gameObject;
+
+        for (int i = 0; i < 10; i++) {
+            GameObject tempbar = Instantiate(Bar, pool.transform.position, Quaternion.identity);
+            obstacleQueue.Add(tempbar);
+        }
+
+        //foreach (GameObject tempbar in obstacleQueue)
+        //{
+        //   print(tempbar);
+       // }
+
+        StartCoroutine("Runspawner");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(0, 0, Player.transform.position.z + 150);
+
+
+    }
+
+    private IEnumerator Runspawner() {
+
+        while (true) {
+            Spawn();
+            yield return new WaitForSeconds(spawnInterval);  
+        }
+
+
+        
+    }
+
+    private void Spawn()
+    {
+        GameObject obstacleToSpawn = obstacleQueue.ElementAt(0);  //Mimics a FIFO queue but allows me to shuffle when changing levels and potentially adding new obstacles
+        //print(obstacleToSpawn.name);
+        obstacleQueue.RemoveAt(0);
+        obstacleQueue.Add(obstacleToSpawn);
+
+        float x = Random.Range(transform.position.x - 20f, transform.position.x + 20f);
+        float y = Random.Range(transform.position.y - 10f, transform.position.y + 10f);
+        float rotation = Random.Range(0f, 180f);
+
+        obstacleToSpawn.transform.position = new Vector3(x, y, transform.position.z);
+        obstacleToSpawn.transform.rotation = Quaternion.Euler(0, 0, rotation);
+    }
+}
